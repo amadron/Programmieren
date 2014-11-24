af6ee2a3d0c52765122e84b7686fbd1de40ee224
beroth <benedict.roth@htwg-konstanz.de>
2014-11-24 20:22:14 +0100

    GraphTraversion breadthSearch and depthSearch.

--- Aufgabe2/src/graph/GraphTraversion.java
+++ Aufgabe2/src/graph/GraphTraversion.java
@@ -1,24 +1,36 @@
 package graph;
+import java.util.AbstractSet;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 
 public class GraphTraversion<V> {
 	//returns a List of the depth of node s
 	public <V> List<V> depthFirstSearch(Graph<V> g, V s){
-		LinkedList<V> visitList = new LinkedList<V>();
-		visitList.add(s);
+		List<V> visitList = new LinkedList<V>();
 		depthFirstSearchR(g, s, visitList);
 		
 		return visitList;
 	}
-	private void depthFirstSearchR(Graph<V> g, V s, LinkedList<V> visitList){
-		List<Edge<V>> eList = g.getIncidentEdgeList(s);
-		for(int i = 0; i < eList.size(); i++){
-			if(!visitList.contains(eList.get(i).target)){
-				visitList.add(eList.get(i).target);
-				if(g.getIncidentEdgeList(eList.get(i).target).size() > 0){
-					depthFirstSearchR(g, eList.get(i).target, visitList);
+	private void depthFirstSearchR(Graph<V> g, V s, List<V> visitList){
+		Stack<V> stk = new Stack<V>();
+		stk.push(s);
+		
+		while(!stk.empty()){
+			V v = stk.pop();
+			if(visitList.contains(v)){
+				continue;
+			}
+			
+			visitList.add(v);
+			
+			List<V> tmpList = g.getAdjacentVertexList(v);
+			for(int i = 0; i < tmpList.size(); i++){
+				if(!visitList.contains(tmpList.get(i))){
+					stk.push(tmpList.get(i));
 				}
 			}
 		}
@@ -26,7 +38,32 @@ public class GraphTraversion<V> {
 	
 	//returns a List of the breadth of node s
 	<V> List<V> breadthFirstSearch(Graph<V> g, V s){
-		return g.getAdjacentVertexList(s);
+		List<V> visitList = new LinkedList<V>();
+		breadthFirstSearchR(g, s, visitList);
+		
+		return visitList;
+	}
+	
+	private void breadthFirstSearchR(Graph<V> g, V s, List<V> visitList){
+				Queue<V> vQueue = new LinkedList<V>();
+				vQueue.add(s);
+				
+				while(!vQueue.isEmpty()){
+					V v = vQueue.remove();
+					
+					if(visitList.contains(v)){
+						continue;
+					}
+					
+					visitList.add(v);
+					
+					List<V> tmpList =  g.getAdjacentVertexList(v);
+					for(int i = 0; i < tmpList.size(); i++){
+						if(!visitList.contains(tmpList.get(i))){
+							vQueue.add(tmpList.get(i));
+						}
+					}
+				}
 	}
 	
 	//returns topological Sort of directed graph as a List
