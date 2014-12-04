2d68f9b65e8d1187a9ada2e52cd552b81fdc2c9d
beroth <benedict.roth@htwg-konstanz.de>
2014-12-04 09:40:42 +0100

    Dijkstra shortestPath

--- Aufgabe2/src/graph/DijkstraShortestPath.java
+++ Aufgabe2/src/graph/DijkstraShortestPath.java
@@ -1,4 +1,6 @@
 package graph;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DijkstraShortestPath<V> {
@@ -17,5 +19,48 @@ public class DijkstraShortestPath<V> {
 	
 	public double getDistance(){
 		return 0;
+	}
+	
+	void shortestPath(V s, Graph<V> G,HashMap<V, Double> d, HashMap<V, V> p){
+		LinkedList<V> kl = new LinkedList<V>();
+		List<Edge<V>> edgeList = G.getEdgeList();
+ 		for(V vert : G.getVertexList()){
+			d.put(vert, 99999999999999999.9999999);
+			p.put(vert, null);
+		}
+		d.put(s, 0.0);
+		kl.add(s);
+		
+		while(!kl.isEmpty()){
+			V v = kl.get(0);
+			for (int i = 0; i < kl.size(); i++){
+				if(kl.get(i) < d.get(v))
+				{
+					
+				}
+			}
+			double weight = G.getIncidentEdgeList(v).get(0).weight;
+			for(Edge<V> edge: G.getIncidentEdgeList(v)){
+				if(edge.weight < weight)
+					weight = edge.weight;
+			}
+			List<V> adjaList = G.getAdjacentVertexList(v);
+			for(V w: adjaList){
+				Edge c = null;
+				for(Edge e: edgeList)
+				{
+					if(e.source == v && e.target == w){
+						c = e;
+					}
+				}
+				if(d.get(w) == null){
+					kl.add(w);
+				}
+				if((d.get(v) + c.weight) < d.get(w)){
+					p.put(v, w);
+					d.put(v, c.weight);					
+				}
+			}
+		}
 	}
 }
--- Aufgabe2/src/graph/DijkstraTest.java
+++ Aufgabe2/src/graph/DijkstraTest.java
@@ -1,5 +1,49 @@
 package graph;
 
-public class DijkstraTest {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class DijkstraTest {
+	
+	public static void main(String[] args){
+		
+	}
+	
+	public static AdjencyListUndirectedGraph<Integer> Fileread(File f){
+		AdjencyListUndirectedGraph<Integer> graph = new AdjencyListUndirectedGraph<Integer>();
+		LineNumberReader in = null;
+		try{
+			in = new LineNumberReader(new FileReader(f));
+				String line;
+			while ((line= in.readLine())!=null){
+				String[] sf = line.split(" ");
+				if(sf.length == 3){
+					double weight = 0;
+					if(sf[2].equals("UBahn")){
+						weight = 5;
+					} else if (sf[2].equals("Taxi")){
+						weight = 3;
+					} else if (sf[2].equals("Bus")){
+						weight = 2;
+					}
+					graph.addVertex(Integer.valueOf(sf[0]));
+					graph.addVertex(Integer.valueOf(sf[1]));
+					graph.addEdge(Integer.valueOf(sf[0]),Integer.valueOf(sf[1]), weight);
+				}
+				
+			}
+			in.close();
+		} catch (IOException ex){
+			Logger.getLogger(TaxiDrive.class.getName()).log(Level.SEVERE,
+					null, ex);
+			
+		}
+		
+		return graph;
+	}
+	
 }
