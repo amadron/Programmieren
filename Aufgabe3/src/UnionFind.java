b3c8ac42da70bdbf185e2ebeccb0bb6a9b743174
Benedict Roth <benedict-roth@live.de>
2014-12-27 22:47:43 +0100

    Created Unionfind.

---
+++ Aufgabe3/src/UnionFind.java
@@ -0,0 +1,28 @@
+
+public class UnionFind {
+	
+	/* Legt eine neue Union-Find-struktur
+	 * mit der Partition {{1},{2},...,{n}} an */
+	public UnionFind(int n){
+		
+	}
+	
+	//Sucht die Menge, zu der e gehört
+	public int find(int e){
+		
+		return 0;
+	}
+	
+	/*Liefert die Anzahl der Mengen in
+	* der Partition zurück */
+	public int size(){
+		
+		return 0;
+	}
+	
+	/*Vereinigt die Menge, zu der e1 gehört
+	 * mit der Menge zu der e2 gehört */
+	public void union (int e1, int e2){
+		
+	}
+}
